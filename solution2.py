63269219ee83a9fd674754842a717c46866679be
DanielMalMed <Danielmalmed@outlook.com>
2023-04-19 19:48:53 -0700

    halway

--- solution2.py
+++ solution2.py
@@ -1,1 +1,10 @@
-# Write code for algorithm 2 below
+def natural_numbers(n,i=1):
+	#your code here
+    if i > n:
+        return
+
+    else:
+        print(i)
+        natural_numbers(n,i+1)
+natural_numbers(3)
+#output: 1 2 3
--- solution4.py
+++ solution4.py
@@ -1,1 +1,11 @@
 # Write code for algorithm 4 below
+# didnt get to so copied from solution code
+def a_to_b(a,b):
+  if b < 1:
+    print("invalid input")
+  elif b == 1:
+    return a
+  else:
+    return a * a_to_b(a,b-1)
+
+print("2^4:")
